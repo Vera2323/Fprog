e5d18db6b9fddee103662ab99d448bd45503dd4c
Vera2323 <121041818+Vera2323@users.noreply.github.com>
2022-12-22 01:44:02 +0800

    Add files via upload

---
+++ lab 3/Problem81.hs
@@ -0,0 +1,10 @@
+
+
+
+paths :: Eq a =>a -> a -> [(a,a)] -> [[a]] 
+paths source sink edges 
+    | source == sink = [[sink]]
+    | otherwise = [
+        source:path | edge<-edges, (fst edge) == source,
+        path<-(paths (snd edge) sink [e|e<-edges, e/=edge])
+    ];
